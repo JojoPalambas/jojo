0fbe920e89b05e51b65c8cf78d8332be6241f75b
Gabriel Depotte <depott_g@epita.fr>
2017-11-07 16:54:08 +0100

    Finished disgusting sort

--- disgusting/disgusting_sort.c
+++ disgusting/disgusting_sort.c
@@ -1,21 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <err.h>
+#include <sys/types.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <errno.h>
+#include <stddef.h>
+#include <stddef.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
-void sort(int *tab, int count)
-{
-    for (int i = 0; i < count; i++)
-    {
-        int min = i;
-        for (int j = i; j < count; j++)
-        {
-            if (tab[j] < tab[min])
-                min = j;
-        }
-        int tmp = tab[i];
-        tab[i] = tab[min];
-        tab[min] = tmp;
-    }
-}
+void function(int *var1, int var2)                                          {
+int *var3 = malloc(sizeof (int) * (var2 + 8));
+var3[0] = 0;
+for (var3[0] = 0; var3[0] < var2 + 8; var3[0]++)                            {
+if (var3[0] < 8)
+continue;
+else if (var3[0] >= 8)
+var3[var3[0]] = var1[var3[0] - 8];                                          }
+var3[1] = -1;
+var3[2] = 0;
+for (var3[0] = 0; var3[0] < var2; var3[0]++)                                {
+var3[3] = 0;
+var3[4] = var3[0];
+var3[5] = 0;
+for (var3[5] = 0; var3[5] < var2; var3[5]++)                                {
+if (var3[3] >= var3[2])                                                     {
+if (var3[var3[5] + 8] < var3[var3[4] + 8])                                  {
+var3[4] = var3[5];                                                          }}
+else                                                                        {
+if (var3[var3[5] + 8] == var3[var3[1] + 8])
+var3[3]++;                                                                  }}
+var3[6] = var3[var3[0] + 8];
+var3[var3[0] + 8] = var3[var3[4] + 8];
+var3[var3[4] + 8] = var3[6];
+if (var3[1] == -1 || var3[var3[0] + 8] != var3[var3[1] + 8])                {
+var3[1] = var3[0];
+var3[2] = 1;                                                                }
+else                                                                        {
+var3[2] += 1;                                                               }}
+for (var3[0] = 0; var3[0] < var2 + 8; var3[0]++)                            {
+if (var3[0] < 8)
+continue;
+else if (var3[0] >= 8)
+var1[var3[0] - 8] = var3[var3[0]];                                          }}
+
+// else if instead of else
+// all variables in a larger copy of tab
+// useless comments in the middle of a line
 
 int main()
 {
@@ -28,7 +63,7 @@ int main()
         printf("%d", tab[i]);
     printf("\n");
 
-    sort(tab, 10);
+    function(tab, 10);
 
     for (int i = 0; i < 10; i++)
         printf("%d", tab[i]);
